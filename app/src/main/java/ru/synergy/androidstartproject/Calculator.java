72c122135c8ef570d1c6f03cb6177b98bdbf667b
Inthesky94 <a.andreev1994@gmail.com>
2023-03-07 11:05:25 +0700

    Lifecycle of activity

--- app/src/main/java/ru/synergy/androidstartproject/Calculator.java
+++ app/src/main/java/ru/synergy/androidstartproject/Calculator.java
@@ -3,6 +3,7 @@ package ru.synergy.androidstartproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,10 +12,47 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Calculator extends AppCompatActivity {
+    private static final String LogcatTag = "CALCULATOR_ACTIVITY";
+    private static final String LifecycleTag = "LIFECYCLE";
+
+    public Calculator() {
+        super();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LifecycleTag, "i'm onStart, and i'm started");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(LifecycleTag,"i'm onStop, and i'm started");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LifecycleTag, "I'm onDestroy, and i'm started");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LifecycleTag, "I'm onPause, and i'm started");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LifecycleTag, "i'm onResume, and i'm started");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LifecycleTag, "I'm onCreate, and i'm started");
         setContentView(R.layout.activity_calculator);
 
         final Button calculate = (Button) findViewById(R.id.calc);
@@ -22,6 +60,7 @@ public class Calculator extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+                Log.d(LogcatTag, "Button have been pushed");
                 calculateAnswer();
 
             }
@@ -39,23 +78,41 @@ public class Calculator extends AppCompatActivity {
 
         TextView answer = (TextView) findViewById(R.id.result);
 
+        Log.d(LogcatTag, "All views have been founded");
 
-        float numone = Integer.parseInt(numOne.getText().toString());
-        float numtwo = Integer.parseInt(numTwo.getText().toString());
+
+        float numtwo = 0;
+        float numone = 0;
+        String num1 = numOne.getText().toString();
+        String num2 = numTwo.getText().toString();
+        if(num1.equals("") && num1 != null) {
+        numone = Integer.parseInt(numOne.getText().toString());}
+
+        if(num2.equals("") && num2 != null) {
+            numtwo = Integer.parseInt(numTwo.getText().toString());
+        }
+
+        Log.d(LogcatTag, "Successfully grabbed data from input fields");
+        Log.d(LogcatTag, "numone is: " + numone + " ; "+" numtwo is: " + numtwo);
+
 
         float solution = 0;
 
         if(add.isChecked()){
+            Log.d(LogcatTag, "Operation is add");
             solution = numone + numtwo;
         }
 
         if(sub.isChecked()){
+            Log.d(LogcatTag, "Operation is sub");
             solution = numone - numtwo;
         }
         if(multiple.isChecked()){
+            Log.d(LogcatTag, "Operation is multiple");
             solution = numone * numtwo;
         }
         if(divide.isChecked()){
+            Log.d(LogcatTag, "Operation is divide");
             if(numtwo == 0 ){
                 Toast.makeText(this,"Number two Cannot be zero",Toast.LENGTH_SHORT).show();
                 return;
@@ -63,7 +120,14 @@ public class Calculator extends AppCompatActivity {
             solution = numone / numtwo;
         }
 
+        Log.d(LogcatTag, "The result of operation is: " + solution);
+
+        // Log.wtf() // what a terrible failure == error
+
         answer.setText("The answer is "+ solution);
+
+
+
 
 
 
